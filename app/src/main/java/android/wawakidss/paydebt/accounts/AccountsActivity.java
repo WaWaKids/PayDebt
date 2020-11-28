a3b124b9e44a3cfbfaa1a1567e01fbca3ef32139
WaWaKids <wawakidss@gmail.com>
2020-11-28 17:26:28 +0300

    -Удалены табы, так как было решено, что все долги будут выводиться на одной странице -Обновлен класс Debt, теперь это единый класс для всех долгов, а не абстрактный, как это было раньше

--- app/src/main/java/android/wawakidss/paydebt/accounts/AccountsActivity.java
+++ app/src/main/java/android/wawakidss/paydebt/accounts/AccountsActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.wawakidss.paydebt.R;
-import android.wawakidss.paydebt.accounts.tabs.PagerAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -24,30 +23,6 @@ public class AccountsActivity extends AppCompatActivity {
 
         Log.d(TAG, "lauching this activity");
         FloatingActionButton fab = findViewById(R.id.fab);
-        TabLayout tabs = (TabLayout)findViewById(R.id.tabs);
-        TabItem i_owe_to = (TabItem)findViewById(R.id.i_owe);
-        TabItem owe_to_me = (TabItem)findViewById(R.id.owe_to_me);
-        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
-
-        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabs.getTabCount());
-        viewPager.setAdapter(pagerAdapter);
-
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +31,7 @@ public class AccountsActivity extends AppCompatActivity {
                 startActivity(new Intent(AccountsActivity.this, AddDebtActivity.class));
             }
         });
+
+
     }
 }
--- app/src/main/java/android/wawakidss/paydebt/data/Debt.java
+++ app/src/main/java/android/wawakidss/paydebt/data/Debt.java
@@ -9,18 +9,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public abstract class Debt {
+public class Debt {
 
     private UUID id;
     private String debt;
     private Calendar takenAt;
     private String comment;
     private String agent;
+    public enum Period {
+        WEEK, MONTH, YEAR
+    }
+    private Period period;
 
     public Debt(Calendar takenAt, String debt, String comment, String agent) {
         id = UUID.randomUUID();
         this.takenAt = takenAt;
         this.comment = comment;
         this.agent = agent;
+        this.debt = debt;
     }
 }
